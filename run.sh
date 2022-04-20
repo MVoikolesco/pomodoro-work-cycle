f6327b1e807b09f51697499feef70f7a0c5bd521
mvoikolesco <marcioavj.ti@outlook.com>
2022-04-20 15:40:30 -0300

    Added verification on python version

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-cd /var/www/pomodoro-work-cycle && python main.py
+if [ ! -d "/usr/bin/python3" ]; then
+    cd /var/www/pomodoro-work-cycle && python3 main.py
+else
+  cd /var/www/pomodoro-work-cycle && python main.py
+fi
